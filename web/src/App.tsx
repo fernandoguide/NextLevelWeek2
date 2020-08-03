2e9090a082dda3fb38aa1b476b165fe25db82b49
fernandoguide <fernando-guide@hotmail.com>
2020-08-03 11:58:22 -0300

    primeiro tela

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/styles/global.css'
+// JSX = JavaScript + XML 
+import Landing from './pages/Landing'
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
